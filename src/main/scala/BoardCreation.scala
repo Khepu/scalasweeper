82b4f31245b55016d3a1e36bf5f1f55a16605c57
Giorgos Makris <gmakris@datascouting.com>
2021-02-16 22:41:52 +0200

    Feature: Partially working board creation

---
+++ src/main/scala/BoardCreation.scala
@@ -0,0 +1,53 @@
+package org.gmakris.scalasweeper
+
+import Entities.Point
+
+import scala.util.{Random, Try}
+
+object BoardCreation {
+
+    // ===================================================================
+    // Core
+    // ===================================================================
+
+    // TODO: make it work
+    def create(width: Int, height: Int, mines: Int): Array[Int] = {
+        val minePoints = createMinePoints(width, height, mines)
+        /*
+        (0 until (width * height))
+            .toArray
+            .map(index =>)
+                         */
+    }
+
+    // ===================================================================
+    // Randomness
+    // ===================================================================
+
+    val rnd = new Random(System.currentTimeMillis())
+
+    def randomValue(max: Int): Try[Int] = {
+        Try(rnd.between(0, max))
+    }
+
+    def randomPoint(maxX: Int, maxY: Int): Try[Point] =
+        for {
+            x <- randomValue(maxX)
+            y <- randomValue(maxY)
+        } yield Point(x, y)
+
+    // ===================================================================
+    // Mines
+    // ===================================================================
+
+    // TODO: make it work
+    def createMinePoints(width: Int, height: Int, mines: Int): Set[Point] = {
+        val mineGenerator = Iterator.unfold(Set.empty[Try[Point]]) {
+            points => randomPoint(width, height) + points
+        }
+            .takeWhile(points => points.size < mines)
+
+    }
+
+
+}
