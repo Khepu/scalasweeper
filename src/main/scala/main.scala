8c8dd4be6c23de99b2a4f6d1374f83a2d45a19d2
Giorgos Makris <gmakris@datascouting.com>
2021-02-20 14:57:13 +0200

    Feature: Board printing

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,28 @@
 package org.gmakris.scalasweeper
 
+import BoardCreation.createBoard
+import Entities.{Config, Tile}
+
 object main {
+    def boardToString(tiles: Array[Array[Tile]]): String =
+        tiles
+            .map(row => row
+                .map(tile =>
+                    if (!tile.isRevealed)
+                        " "
+                    else if (tile.value == -1)
+                        "*"
+                    else
+                        tile.value.toString))
+            .map(_.mkString("|"))
+            .mkString("\n")
+
     def main(args: Array[String]): Unit = {
-        println("Hello, world")
+        val config = Config(10, 15, 10)
+
+        for {
+            board <- createBoard(config)
+            tiles = board.tiles
+        } yield print(boardToString(tiles))
     }
 }
